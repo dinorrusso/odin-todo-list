8fa8d552b73c96f8f7efab58c4d9f113fb9bf1b8
dinorrusso <drr.developer.1972@gmail.com>
2025-08-18 08:35:28 -0400

    Updates

--- src/UtilityFunctions.js
+++
@@ -1,9 +0,0 @@
-export function getFormattedDate(){
-  const today = new Date();
-  const yyyy = today.getFullYear();
-  const mm = String(today.getMonth() + 1).padStart(2, '0'); // months are zero-based
-  const dd = String(today.getDate()).padStart(2, '0');
-  const formattedDate = `${yyyy}-${mm}-${dd}`;
-  return formattedDate;
-}
-
